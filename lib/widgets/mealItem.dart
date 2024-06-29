7d3156b0c18db861811391be6c0aedf076b67232
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-06-30 02:55:31 +0300

    card

--- lib/widgets/mealItem.dart
+++ lib/widgets/mealItem.dart
@@ -10,17 +10,45 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 3,
+      clipBehavior: Clip.antiAlias,
       child: InkWell(
         child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
           children: [
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
             ),
-            Center(
-              child: Text(
-                meal.title,
-                style: TextStyle(color: Colors.black, fontSize: 20),
+            Positioned(
+              left: 0,
+              bottom: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: true,
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [],
+                    )
+                  ],
+                ),
               ),
             )
           ],
