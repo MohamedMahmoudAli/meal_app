5e734e268f8c099e6da49fa4bfc31a6f062b5f90
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-06-29 17:45:58 +0300

    create catogries screen

---
+++ lib/widgets/categories_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Data'),
+      ),
+      body: GridView(
+        gridDelegate:
+            const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 100),
+      ),
+    );
+  }
+}
